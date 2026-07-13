55f1c655d3aaf567ae852e4b6e72f874bb366859
caiquemurilo <caiquemurilo@gmail.com>
2026-07-13 20:24:21 -0300

    refactor: extract Header, Main, and Footer components
    - Move App.jsx to the new src/components directory
    - Create Header component in its own folder and extract header layout
    - Create Main and Footer components in their respective folders
    - Integrate new components into App.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import logo from '../../images/logo.svg'
+function Header() {
+  return (
+    <header className="header page__section">
+      <img
+        alt="Around The U.S. logo"
+        className="logo header__logo"
+        src={logo}
+      />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,37 @@
+import avatar from '../../images/avatar.jpg'
+function Main() {
+  return (
+    <main className="content">
+      <section className="profile page__section">
+        <div className="profile__avatar">
+          <img className="profile__image" src={avatar} alt="Avatar" />
+          <div className="profile__overlay">
+            <img
+              src="./images/edit-icon.svg"
+              alt="Edit Icon"
+              className="profile__overlay-icon"
+            />
+          </div>
+        </div>
+        <div className="profile__info">
+          <h1 className="profile__title">Name Surname</h1>
+          <button
+            aria-label="Editar perfil"
+            className="profile__edit-button"
+            type="button"
+          ></button>
+          <p className="profile__description">Description</p>
+        </div>
+        <button
+          aria-label="Adicionar cartão"
+          className="profile__add-button"
+          type="button"
+        ></button>
+      </section>
+      <section className="cards page__section">
+        <ul className="cards__list"></ul>
+      </section>
+    </main>
+  )
+}
+export default Main
