503aa7520571d385c0d598fbf6087c46f8a385a5
caiquemurilo <caiquemurilo@gmail.com>
2026-07-16 15:29:43 -0300

    fix: change class to className in ImagePopup to resolve React console warning

--- src/components/Main/components/Popup/components/ImagePopup/ImagePopup.jsx
+++ src/components/Main/components/Popup/components/ImagePopup/ImagePopup.jsx
@@ -1,7 +1,7 @@
 export default function ImagePopup({ name, link }) {
   return (
     <>
-      <img alt={name} class="popup__image" src={link} />
+      <img alt={name} className="popup__image" src={link} />
       <p class="popup__caption">{name}</p>
     </>
   )
